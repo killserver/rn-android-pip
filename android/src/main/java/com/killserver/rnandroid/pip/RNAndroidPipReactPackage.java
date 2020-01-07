6b5bdd60b5fc7599429b282b19b24a6675d08fc2
killer <killer-server@mail.ru>
2020-01-07 22:45:00 +0200

    Update RNAndroidPipReactPackage.java

--- android/src/main/java/com/killserver/rnandroid/pip/RNAndroidPipReactPackage.java
+++ android/src/main/java/com/killserver/rnandroid/pip/RNAndroidPipReactPackage.java
@@ -12,18 +12,21 @@ import java.util.Collections;
 import java.util.List;
 
 public class RNAndroidPipReactPackage implements ReactPackage {
-    @Override
-    public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
-      return Arrays.<NativeModule>asList(new RNAndroidPipModule(reactContext));
-    }
 
-    // Deprecated from RN 0.47
-    public List<Class<? extends JavaScriptModule>> createJSModules() {
-      return Collections.emptyList();
-    }
+  @Override
+  public List<Class<? extends JavaScriptModule>> createJSModules() {
+    return Collections.emptyList();
+  }
 
-    @Override
-    public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
-      return Collections.emptyList();
-    }
+  @Override
+  public List<ViewManager> createViewManagers(ReactApplicationContext reactContext) {
+    return Collections.emptyList();
+  }
+
+  @Override
+  public List<NativeModule> createNativeModules(ReactApplicationContext reactContext) {
+    List<NativeModule> modules = new ArrayList<>();
+    modules.add(new RNAndroidPipModule(reactContext));
+    return modules;
+  }
 }
